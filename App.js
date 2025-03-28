4e01ee2d2ca01ac5bb2a93f021fb2537f48f41c4
imohitsikarwar <145597855+imohitsikarwar@users.noreply.github.com>
2025-03-28 02:45:58 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './DetailsScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-    return ( <
-        NavigationContainer >
-        <
-        Stack.Navigator >
-        <
-        Stack.Screen name = "Home"
-        component = { HomeScreen }
-        /> <
-        Stack.Screen name = "Details"
-        component = { DetailsScreen }
-        /> <
-        /Stack.Navigator> <
-        /NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
